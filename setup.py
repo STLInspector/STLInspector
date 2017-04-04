7f5a4f43e8f6c3ca644b8b2b14586ef66db4508e
Hendrik Roehm <git@roehm.ws>
2017-04-04 16:44:34 +0200

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='STLInspector',
-    version='0.1.0',
+    version='1.0.0',
     description='Systematic validation of Signal Temporal Logic (STL) specifications against informal textual requirements.',
     classifiers=[
         'Development Status :: 4 - Beta',
